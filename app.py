5c62b87b67cdaec95212f8917abc041b10166fed
Siddharth <81458908+PS2307@users.noreply.github.com>
2022-09-24 11:13:36 +0530

    secondcommit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("\reva")
+@app.route("/reva")
 def reva_home():
     return{"data": "It's created."}
+@app.route("/image")
+def reva_home():
+    return render_template("home.html")
 
 if __name__=="__main__":
     app.run(debug=True)
