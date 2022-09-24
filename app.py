65c7c42046da34d7d4a034280f5f69383d9237db
Siddharth <81458908+PS2307@users.noreply.github.com>
2022-09-24 10:44:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/reva")
 def reva_home():
-    return {"data":"It's created"}
+    return{"data": "It's created."}
 
-@app.route("/image")
-def access_image():
-    return render_template("home.html")
-if __name__ == "_main_":
-    app.run(host="0.0.0.0", port=5000)
+if __name__=="__main__":
+    app.run(debug=True)
